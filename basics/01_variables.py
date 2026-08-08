4f7f59ad42e414b89606a0b44a8df1647ff28b8c
hemashri0215-lang <hemashri0215@gmail.com>
2026-08-08 21:06:28 +0530

    Update 01_variables.py

--- basics/01_variables.py
+++ basics/01_variables.py
@@ -6,14 +6,11 @@ course = "B.Tech IT"
 university = "Pondicherry University"
 gpa = 8.5
 is_graduating_soon = False
-
-
 print("Student Name:", name)
 print("Age:", age)
 print("Course:", course, "- Year", year_of_study)
 print("University:", university)
 print("Current GPA:", gpa)
 print("Graduating this year?", )
-
 years_left = 4 - year_of_study
 print("Years left to graduate:", years_left)
