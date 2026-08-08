b571cad2372e4b428e8df83b0878817c5cfe8033
hemashri0215-lang <hemashri0215@gmail.com>
2026-08-08 21:05:36 +0530

    Create basics/01_variables.py

---
+++ basics/01_variables.py
@@ -0,0 +1,19 @@
+
+name = "Hema"
+age = 21
+year_of_study = 3
+course = "B.Tech IT"
+university = "Pondicherry University"
+gpa = 8.5
+is_graduating_soon = False
+
+
+print("Student Name:", name)
+print("Age:", age)
+print("Course:", course, "- Year", year_of_study)
+print("University:", university)
+print("Current GPA:", gpa)
+print("Graduating this year?", )
+
+years_left = 4 - year_of_study
+print("Years left to graduate:", years_left)
